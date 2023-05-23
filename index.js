caee951604d17e3d837cded2812b0b2752a5d247
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-05-23 23:55:53 +0300

    Creation of console applications

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Welcome to Node.js")
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const contacts = require("./contacts");
+
+const invokeActions = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeActions(argv);
